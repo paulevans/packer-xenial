16b13e0bec786065dfa7cf4748911b1114ec5f84
Paul Evans <pevans@certainaffinity.com>
2017-02-14 11:22:12 -0600

    Fixed line endings - bash needs things to be always lf.  Windows needs to be checked out as core.autocrlf=input

--- shell/install-app.sh
+++ shell/install-app.sh
@@ -6,7 +6,7 @@
 # EventStore
 ES_VERSION=3.9.3
 ES_PRERELEASE_VERSION=4.0.0-alpha3
-# ES_USE_PRERELEASE=1
+ES_USE_PRERELEASE=1
 
 chown root:root /home/vagrant/tmp.eventstore.conf
 chmod 644 /home/vagrant/tmp.eventstore.conf
@@ -15,11 +15,13 @@ mv /home/vagrant/tmp.eventstore.conf /etc/eventstore.conf
 apt-get install -y curl 
 
 if [ -z ${ES_USE_PRERELEASE+x} ]; then 
+    echo Use EventStore-OSS release
     curl -s https://packagecloud.io/install/repositories/EventStore/EventStore-OSS/script.deb.sh | sudo bash 
     apt-get install -y eventstore-oss=$ES_VERSION
 else 
+    echo Use EventStore-OSS pre-release
     curl -s https://packagecloud.io/install/repositories/EventStore/EventStore-OSS-PreRelease/script.deb.sh | sudo bash
-    apt-get install -y eventstore-oss=ES_PRERELEASE_VERSION
+    apt-get install -y eventstore-oss=$ES_PRERELEASE_VERSION
 fi
 
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
